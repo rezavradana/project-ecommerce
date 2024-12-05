d47bd6b22ed0f4ae066a5809c9c89200324675f2
rezavradana <rezapradana472@gmail.comecho>
2024-12-05 12:20:49 +0700

    make cart and wishlist

--- src/scripts/views/pages/cart.js
+++ src/scripts/views/pages/cart.js
@@ -1,6 +1,67 @@
 const Cart = {
     async render() {
+        const mainContent = document.querySelector('#maincontent');
 
+        mainContent.innerHTML = `
+        <div class="cart-products">
+        <div class="wrapper-cart">
+            <div class="cart-item">
+                <div class="cart-image">
+                    <span class="cart-discount">23%</span>
+                    <img src="">
+                </div>
+                <div class="cart-details">
+                    <p class="cart-title">PONDS Men Acne Solution Facial Foam 50g</p>
+                    <div class="cart-price">
+                    <span class="cart-current-price">Rp28.500</span>
+                    <span class="cart-original-price">Rp37.200</span>
+                    </div>
+                </div>
+                <div class="cart-actions">
+                    <button class="cart-action-btn"><i class="fa fa-trash"></i></button>
+                    <div class="cart-quantity-control">
+                    <button class="cart-quantity-btn">-</button>
+                    <input type="text" class="cart-quantity-input" value="1" readonly>
+                    <button class="cart-quantity-btn">+</button>
+                    </div>
+                </div>
+            </div>
+            <div class="cart-item">
+                <div class="cart-image">
+                    <span class="cart-discount">23%</span>
+                    <img src="">
+                </div>
+                <div class="cart-details">
+                    <p class="cart-title">PONDS Men Acne Solution Facial Foam 50g</p>
+                    <div class="cart-price">
+                    <span class="cart-current-price">Rp28.500</span>
+                    <span class="cart-original-price">Rp37.200</span>
+                    </div>
+                </div>
+                <div class="cart-actions">
+                    <button class="cart-action-btn"><i class="fa fa-trash"></i></button>
+                    <div class="cart-quantity-control">
+                    <button class="cart-quantity-btn">-</button>
+                    <input type="text" class="cart-quantity-input" value="1" readonly>
+                    <button class="cart-quantity-btn">+</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+            <div class="cart-transactions">
+                <div class="title-transactions">
+                    <p class="title">Ringkasan Belanja</p>
+                    <div class="price">
+                        <p>Total</p>
+                        <p class="total-price">Rp28.500</p>
+                    </div>
+                </div>
+                <div class="button-transactions">
+                    <button class="cart-transactions-btn">Beli</button>
+                </div>
+            </div>
+        </div>
+        `;
     },
 
     async afterRender() {
--- src/scripts/views/pages/wishlist.js
+++ src/scripts/views/pages/wishlist.js
@@ -1,6 +1,37 @@
 const Wishlist = {
     async render() {
+        const mainContent = document.querySelector('#maincontent');
 
+        mainContent.innerHTML = `
+        <div class="wishlist-products"> 
+            <div class="product-card">
+                <div class="product-discount">-35%</div>
+                <button class="product-trash">
+                    <i class="fa fa-trash"></i>
+                </button>
+                <img src="" class="product-image" />
+                <button class="product-cart-btn">Add To Cart</button>
+                <h3 class="product-title">Gucci duffle bag</h3>
+                <div class="product-price">
+                    <span class="current-price">$960</span>
+                    <span class="original-price">$1160</span>
+                </div>
+            </div>
+            <div class="product-card">
+                <div class="product-discount">-35%</div>
+                <button class="product-trash">
+                    <i class="fa fa-trash"></i>
+                </button>
+                <img src="" class="product-image" />
+                <button class="product-cart-btn">Add To Cart</button>
+                <h3 class="product-title">Gucci duffle bag</h3>
+                <div class="product-price">
+                    <span class="current-price">$960</span>
+                    <span class="original-price">$1160</span>
+                </div>
+            </div>
+        </div>
+        `;
     },
 
     async afterRender() {
