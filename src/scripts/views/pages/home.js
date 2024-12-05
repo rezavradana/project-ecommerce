843839ec22fbbbdcb18ab514e7592b961e0b4fc2
rezavradana <rezapradana472@gmail.comecho>
2024-12-05 14:01:40 +0700

    add caraousel

--- src/scripts/views/pages/home.js
+++ src/scripts/views/pages/home.js
@@ -1,12 +1,25 @@
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle';
+// import styles bundle
+import 'swiper/css/bundle';
+
 const Home = {
     async render() {
         const mainContent = document.querySelector('#maincontent');
 
         mainContent.innerHTML = `
-        <div class="jumbotron">
-            <div class="slideshow">
-                <img src="" alt="" class="active black">
+        <div class="swiper">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide red">Slide 1</div>
+                <div class="swiper-slide blue">Slide 2</div>
+                <div class="swiper-slide green">Slide 3</div>
             </div>
+            <div class="swiper-pagination"></div>
+
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+
+            <div class="swiper-scrollbar"></div>
         </div>
 
         <div class="category">
@@ -124,7 +137,31 @@ const Home = {
     },
 
     async afterRender() {
-
+        const swiper = new Swiper('.swiper', {
+            // Optional parameters
+            direction: 'horizontal',
+            loop: true,
+            autoplay: {
+                delay: 2000,
+                pauseOnMouseEnter: true,
+            },
+          
+            // If we need pagination
+            pagination: {
+              el: '.swiper-pagination',
+            },
+          
+            // Navigation arrows
+            navigation: {
+              nextEl: '.swiper-button-next',
+              prevEl: '.swiper-button-prev',
+            },
+          
+            // And if we need scrollbar
+            scrollbar: {
+              el: '.swiper-scrollbar',
+            },
+          });
     }
 }
 
