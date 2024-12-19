935ddda9bfc100347edacf0da0e1d832c8d9f1bc
rezavradana <rezapradana472@gmail.comecho>
2024-12-19 23:14:19 +0700

    create categories

--- src/scripts/views/pages/category.js
+++ src/scripts/views/pages/category.js
@@ -1,86 +1,26 @@
 import UrlParser from '../../route/url-parser';
-import { getProducts } from '../../data/main';
+import { getProductFromCategory, getProducts } from '../../data/main';
 import { filterProductsByCategory } from '../../utils/category-filter';
 
 const Category = {
     async render() {
         const { verb } = UrlParser.parseActiveUrlWithoutCombiner();
+        const titleCategory = verb.trim().charAt(0).toUpperCase() + verb.trim().slice(1);
         const mainContent = document.querySelector('#maincontent');
 
         mainContent.innerHTML = `
         <div class="category">
-        <div class="title-category">
-            <div class="header">
-                <i></i>
-                <span>${verb}</span>
+            <div class="title-category">
+                <div class="header">
+                    <i></i>
+                    <span>${titleCategory}</span>
+                </div>
+                <div class="title">
+                    <h1>Browse By ${titleCategory}</h1>
+                </div>
             </div>
-            <div class="title">
-                <h1>Browse By ${verb}</h1>
-            </div>
-        </div>
 
-        <div class="list-products">
-            <div class="product">
-                <a href="/#/product/1"> 
-                    <div class="image-product">
-                        <img src="" alt="">
-                    </div>
-                    <div class="description-product">
-                        <span>Judul Product</span>
-                        <span>Rp.15000</span>
-                        <div class="rating">
-                            <i class="fa fa-star"> 5.0 | </i>
-                            <span>10 terjual</span>
-                        </div>
-                    </div>
-                </a>
-            </div>
-            <div class="product">
-                <a href="/#/product/1"> 
-                    <div class="image-product">
-                        <img src="" alt="">
-                    </div>
-                    <div class="description-product">
-                        <span>Judul Product</span>
-                        <span>Rp.15000</span>
-                        <div class="rating">
-                            <i class="fa fa-star"> 5.0 | </i>
-                            <span>10 terjual</span>
-                        </div>
-                    </div>
-                </a>
-            </div>
-            <div class="product">
-                <a href="/#/product/1"> 
-                    <div class="image-product">
-                        <img src="" alt="">
-                    </div>
-                    <div class="description-product">
-                        <span>Judul Product</span>
-                        <span>Rp.15000</span>
-                        <div class="rating">
-                            <i class="fa fa-star"> 5.0 | </i>
-                            <span>10 terjual</span>
-                        </div>
-                    </div>
-                </a>
-            </div>
-            <div class="product">
-                <a href="/#/product/1"> 
-                    <div class="image-product">
-                        <img src="" alt="">
-                    </div>
-                    <div class="description-product">
-                        <span>Judul Product</span>
-                        <span>Rp.15000</span>
-                        <div class="rating">
-                            <i class="fa fa-star"> 5.0 | </i>
-                            <span>10 terjual</span>
-                        </div>
-                    </div>
-                </a>
-            </div>
-        </div>
+            <div class="list-products"></div>
         </div>
         `
 
@@ -88,37 +28,46 @@ const Category = {
 
     async afterRender() {
         const { verb } = UrlParser.parseActiveUrlWithoutCombiner();
-        const responseJson = await getProducts();
-            if (responseJson.status === 'success' && responseJson.data && responseJson.data.products) {
-                const products = responseJson.data.products;
+        const titleCategory = verb.trim().charAt(0).toUpperCase() + verb.trim().slice(1);
 
-                // Memfilter produk berdasarkan kategori
-                const filteredProducts = filterProductsByCategory(products, verb);
 
-                const listProductsContainer = document.querySelector('.list-products');
-                    listProductsContainer.innerHTML = '';
-                    filteredProducts.forEach(product => {
-                        const productElement = document.createElement('div');
-                            productElement.classList.add('product');
+        const elementProductTemplate = (product) => {
+            const listProductsElement = document.querySelector('.list-products');
+            listProductsElement.innerHTML += `
+            <div class="product">
+                <a href="/#/product/${product.id}"> 
+                    <div class="image-product">
+                        <img src="./images/${product.image_url}" alt="">
+                    </div>
+                    <div class="description-product">
+                        <span>${product.name}</span>
+                        <span>${product.price}</span>
+                        <div class="rating">
+                            <i class="fa fa-star"> 5.0 | </i>
+                            <span>${product.stock}</span>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            `
+        }
 
-                            productElement.innerHTML = `
-                                <a href="/#/product/${product.id}"> 
-                                    <div class="image-product">
-                                        <img src="./images/${product.image_url}" alt="${product.name}">
-                                    </div>
-                                    <div class="description-product">
-                                        <span>${product.name}</span>
-                                        <span>Rp.${product.price}</span>
-                                        <div class="rating">
-                                            <span>Tersisa: ${product.stock} Stok</span>
-                                        </div>
-                                    </div>
-                                </a>
-                            `;
-                        listProductsContainer.appendChild(productElement);
-                    });
-        } else {
-            console.error('Terjadi kesalahan, gagal dalam memuat produk');
+        if (titleCategory === 'Baju') {
+            const resultProductByCategory = await getProductFromCategory('categories-h1fb8xYOwvlZWc_g');
+            const { products } = resultProductByCategory.data;
+
+            for (const product of products) {
+                elementProductTemplate(product);
+            }
+        } 
+
+        if (titleCategory === 'Kerudung') {
+            const resultProductByCategory = await getProductFromCategory('categories-kzsxI8bbn-l15gsM');
+            const { products } = resultProductByCategory.data;
+
+            for (const product of products) {
+                elementProductTemplate(product);
+            }
         }
     }
 }
